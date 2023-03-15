ce84139bb5a6d9bd5007127fc5ace98605807d1e
Mihai Andrei Gherghinescu <mihai.gherghinescu00@e-uvt.ro>
2023-03-15 22:33:58 +0200

    Conversion invalid

--- Testing/Main.cpp
+++ Testing/Main.cpp
@@ -48,6 +48,30 @@ struct command_line
     std::vector<std::wstring> m_arguments;
 };
 
+std::string GetLastErrorAsString()
+{
+    //Get the error message ID, if any.
+    DWORD errorMessageID = ::GetLastError();
+    if (errorMessageID == 0) {
+        return std::string(); //No error message has been recorded
+    }
+
+    LPSTR messageBuffer = nullptr;
+
+    //Ask Win32 to give us the string version of that message ID.
+    //The parameters we pass in, tell Win32 to create the buffer that holds the message for us (because we don't yet know how long the message string will be).
+    size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
+
+    //Copy the error message into a std::string.
+    std::string message(messageBuffer, size);
+
+    //Free the Win32's string's buffer.
+    LocalFree(messageBuffer);
+
+    return message;
+}
+
 // TO BE ADDED INSIDE COMMON
 bool createProcessFromSameDirectory(const command_line& cmd)
 {
@@ -80,8 +104,8 @@ bool createProcessFromSameDirectory(const command_line& cmd)
         &pi)           // Pointer to PROCESS_INFORMATION structure
         )
     {
-        // THIS CAN'T BE OUTPUT GetLastError();
-        LOG_ERR << "CreateProcess failed";
+
+        LOG_ERR << "CreateProcess failed" << GetLastErrorAsString();
         return false;
     }
 
